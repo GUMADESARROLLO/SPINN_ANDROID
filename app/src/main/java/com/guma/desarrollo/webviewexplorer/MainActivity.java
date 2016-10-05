06974fba36c12bdc1f895c518ed369a21a7f1a96
Luis Francisco Perez Mendoza <analista3.guma@gmail.com>
2016-10-05 09:11:06 -0600

    Agregar manejo para que cuando se gire el dispositivo no recargue la pagina nuevamente.

--- app/src/main/java/com/guma/desarrollo/webviewexplorer/MainActivity.java
+++ app/src/main/java/com/guma/desarrollo/webviewexplorer/MainActivity.java
@@ -2,6 +2,7 @@ package com.guma.desarrollo.webviewexplorer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -24,7 +25,22 @@ public class MainActivity extends AppCompatActivity
 
         mWebView.setWebViewClient(new WebViewClient());
         //mWebView.loadUrl("http://beta.html5test.com");
-        mWebView.loadUrl("http://mail.google.com");
+        if (savedInstanceState==null)
+        {mWebView.loadUrl("http://www.laprensa.com.ni");}
+
+    }
+ 
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        mWebView.saveState(outState);
     }
 
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceSatate)
+    {
+        super.onRestoreInstanceState(savedInstanceSatate);
+        mWebView.restoreState(savedInstanceSatate);
+    }
 }
